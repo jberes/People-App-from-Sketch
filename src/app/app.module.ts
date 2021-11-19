feb2b71d8fd4002971b3b5ec13430f5812d9c16c
Jason Beres <jasonberes@gmail.com>
2021-11-19 12:40:47 -0500

    People App from Sketch initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PeopleHomeComponent } from './people-home/people-home.component';
+import { IgxNavbarModule, IgxIconModule, IgxListModule, IgxAvatarModule, IgxBadgeModule, IgxButtonModule, IgxInputGroupModule, IgxDatePickerModule, IgxRadioModule, IgxSliderModule, IgxCheckboxModule, IgxDialogModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { EditPersonComponent } from './edit-person/edit-person.component';
+import { AddPersonComponent } from './add-person/add-person.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleHomeComponent,
+    EditPersonComponent,
+    AddPersonComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxNavbarModule,
+    IgxIconModule,
+    IgxListModule,
+    IgxAvatarModule,
+    IgxBadgeModule,
+    IgxButtonModule,
+    FormsModule,
+    IgxInputGroupModule,
+    IgxDatePickerModule,
+    IgxRadioModule,
+    IgxSliderModule,
+    IgxCheckboxModule,
+    IgxDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
