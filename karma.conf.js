b03b44353e26b82395ba2b5d6060feceab7a57e0
Jason Beres <jasonberes@gmail.com>
2021-11-19 13:11:34 -0500

    People-App 2 updates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/people-app-from-sketch'),
+      dir: require('path').join(__dirname, './coverage/people-app-2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
